4acce6bf76c13d18c2e70d6ff5bddc4b2df3de80
Francisco <franciscoalfonso.dev@gmail.com>
2025-10-06 21:02:14 -0300

    Base de ORM

---
+++ ISW_PRACTICO/ISW_TRABAJOS_PRACTICOS_EVALUABLES/ISW_TP/ISW_TP_006/backend/models/models.js
@@ -0,0 +1,18 @@
+import { DataTypes } from 'sequelize'
+import sequelize from '../config/database.js'
+
+const User = sequelize.define('User', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  }
+}, {
+  tableName: 'users'
+})
+
+export default User
