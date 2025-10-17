4eb04cd082cf040efc6d9fd223ef171e27ce25f1
Francisco <franciscoalfonso.dev@gmail.com>
2025-10-17 18:12:37 -0300

    Test 50% descuentro para tipo VIP y edad entre 4 y 15

--- ISW_PRACTICO/ISW_TRABAJOS_PRACTICOS_EVALUABLES/ISW_TP/ISW_TP_006/backend/tests/unit/services/edad_4_15_vip_50%.spec.js
+++ ISW_PRACTICO/ISW_TRABAJOS_PRACTICOS_EVALUABLES/ISW_TP/ISW_TP_006/backend/tests/unit/services/edad_4_15_vip_50%.spec.js
@@ -1,12 +1,10 @@
-import { calcularPrecioEntrada } from "../../../services/comprarEntradasService";
+import Entrada from "../../../class/Entrada";
 
 describe('Validacion precio de entrada', () => {
 
     test("deberia aplicar 50% de descuento para tipo VIP y edad entre 4 y 15", ()=>{
-        const entrada = {
-            edad : 4,
-            tipo: "vip"
-        }
-        expect(calcularPrecioEntrada(entrada.edad,entrada.tipo).toBe())
+        const entrada = new Entrada(4,"vip")
+        const resultadoEsperado = (entrada.obtenerPrecioBase() * 0.5)
+        expect(entrada.calcularPrecioEntrada(entrada.edad,entrada.tipo).toBe(resultadoEsperado))
     })
 })
